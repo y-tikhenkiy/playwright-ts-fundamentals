30475fff62a730adece2c9d78b16f04a8f3c210d
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-17 13:30:11 +0100

    ui components: radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -21,4 +21,18 @@ test.describe('Forms Layouts page', ()=>{
         //locator assertion
         await expect(usingTheGridEmailInput).toHaveValue('test@test.com');
     })
+
+    test('radio buttons', async({page})=>{
+        const usingTheGridForm = page.locator('nb-card', {hasText: 'Using the Grid'})
+
+        // await usingTheGridForm.getByLabel('Option 1').check({force:true});
+        await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force:true});
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked();
+        expect(radioStatus).toBeTruthy();
+        // await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked();
+
+        await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force:true});
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy();
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy();
+    })
 })
