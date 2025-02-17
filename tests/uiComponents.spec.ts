542ba8fcdce729d100f4ac9b9702a63217936085
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-17 14:33:40 +0100

    ui components: list and dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -52,3 +52,33 @@ test('checkboxes', async({page}) =>{
         expect(await box.isChecked()).toBeTruthy();
     } 
 })
+
+test('list and dropdowns', async({page})=>{
+    const dropDownMenu = page.locator('ngx-header nb-select');
+    await dropDownMenu.click();
+
+    page.getByRole('list'); // when the list is a UL tag
+    page.getByRole('listitem'); // when the list is a LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option');
+    const optionList = page.locator('nb-option-list nb-option'); // alternative with the previous
+    await expect(optionList).toHaveText(['Light', 'Dark', 'Cosmic', 'Corporate']);
+    await optionList.filter({hasText:'Cosmic'}).click();
+    const header = page.locator('nb-layout-header');
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)');
+
+    const colors = {
+        'Light': 'rgb(255, 255, 255)', 
+        'Dark': 'rgb(34, 43, 69)', 
+        'Cosmic': 'rgb(50, 50, 89)', 
+        'Corporate': 'rgb(255, 255, 255)'
+    }
+
+    await dropDownMenu.click();
+    for(const color in colors){
+        await optionList.filter({hasText: color}).click();
+        await expect(header).toHaveCSS('background-color', colors[color]);
+        if(color != 'Corporate')
+            await dropDownMenu.click(); 
+    }
+})
