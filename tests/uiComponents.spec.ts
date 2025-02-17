3560dcd60581a34c6a314962933e9382466f1393
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-17 23:23:33 +0100

    ui components: web tables 2

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -130,5 +130,25 @@ test('web tables', async({page})=>{
 
     await expect(targetRowByID.locator('td').nth(5)).toHaveText('test@test.com')
 
+    //3 test filter of the table
+
+    const ages = ['20', '30', '40', '200'];
+    
+    for(let age of ages){
+        await page.locator('input-filter').getByPlaceholder('Age').clear();
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age);
+        await page.waitForTimeout(500);
+        const ageRows = page.locator('tbody tr');
+
+        for(let row of await ageRows.all()){
+            const cellValue = await row.locator('td').last().textContent();
+            
+            if(age === '200'){
+                expect(await page.getByRole('table').textContent()).toContain('No data found');
+            } else {
+                expect(cellValue).toEqual(age);
+            }
+        }
+    }
 
 })
