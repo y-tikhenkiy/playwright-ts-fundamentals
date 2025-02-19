0b5c0a86d02600f75982ee8c64a780c7241b2eeb
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-19 12:56:25 +0100

    ui components: datepicker p.2

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -153,13 +153,43 @@ test('web tables', async({page})=>{
 
 })
 
-test('datepicker', async({page})=>{
-    await page.getByText('Forms').click();
-    await page.getByText('Datepicker').click();
+test('datepicker p.1', async({page})=>{
+await page.getByText('Forms').click();
+await page.getByText('Datepicker').click();
 
-    const calendarInputField = page.getByPlaceholder('Form Picker');
+const calendarInputField = page.getByPlaceholder('Form Picker');
     await calendarInputField.click();
     await page.locator('[class="day-cell ng-star-inserted"]').getByText('1', {exact: true}).click(); // exact: true is for exact match
 
     expect(calendarInputField).toHaveValue('Feb 1, 2025');
+})
+
+test('datepicker p.2', async({page})=>{
+    await page.getByText('Forms').click();
+    await page.getByText('Datepicker').click();
+
+    let date = new Date();
+    date.setDate(date.getDate()+514);
+    const expectedDate = date.getDate().toString();
+    const expectedMonthShort = date.toLocaleString('En-US', {month: 'short'});
+    const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'});
+    const expectedYear = date.getFullYear();
+    const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+    
+    const calendarInputField = page.getByPlaceholder('Form Picker');
+    await calendarInputField.click();
+    
+    let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent();
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`;
+
+    while(!calendarMonthAndYear.includes(expectedMonthAndYear)){
+        await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click();
+        calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent();
+    }
+    
+    
+    
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click(); // exact: true is for exact match
+    
+    expect(calendarInputField).toHaveValue(dateToAssert);
 })
