e8950e025cad2b3c748e4cb5326c5223d48f4f56
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-21 00:06:54 +0100

    page object: paramethrized methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import{NavigatoinPage} from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPaga';
 
 test.beforeEach(async({page})=>{
     await page.goto('http://localhost:4200/');
@@ -12,4 +13,13 @@ test('navigate to forms page', async({page})=>{
     await navigateTo.smartTablePage();
     await navigateTo.toastrPage();
     await navigateTo.tooltipPage();
+})
+
+test('paramethrized methods', async({page})=>{
+    const navigateTo = new NavigatoinPage(page);
+    const onFormLayoutsPage = new FormLayoutsPage(page);
+
+    await navigateTo.formLayoutsPage();
+    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectingOption('test@test.com','123123','Option 1');
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'jhon95@test.com', true);
 })
